a228a40458e60d220baff5228d51d08846e89797
Muhammad Adeel Chaudhary <83158277+madeelchaudhary@users.noreply.github.com>
2023-09-25 11:55:04 +0500

    Enabled strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,10 @@ import App from "./App.tsx";
 import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <ChakraProvider theme={theme}>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <App />
-  </ChakraProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
 );
